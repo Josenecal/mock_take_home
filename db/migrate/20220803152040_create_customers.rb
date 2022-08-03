518f3a2e1419e1a59248da7eb3ad5af5538c3ea1
Joseph Senecal <jasenecal2@gmail.com>
2022-08-03 10:02:59 -0600

    fix: missing text from migration preventing execution

--- db/migrate/20220803152040_create_customers.rb
+++ db/migrate/20220803152040_create_customers.rb
@@ -3,7 +3,8 @@ class CreateCustomers < ActiveRecord::Migration[5.2]
     create_table :customers do |t|
       t.string :first_name
       t.string :last_name
-      t.stringaddress :email
+      t.string :address 
+      t.string :email
       t.timestamps
     end
   end
