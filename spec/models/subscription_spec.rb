da147fbf75cd32da9e1ac52837ea0529fad5e83e
Joseph Senecal <jasenecal2@gmail.com>
2022-08-03 14:19:49 -0600

    Test: add tests for remaining models

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations and relations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :frequency }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :status }
+
+    it { should belong_to :customer }
+    it { should belong_to :tea }
+  end
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relations and validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :brew_time }
+
+    it { should have_many :subscriptions }
+    it { should have_many :customers }
+  end
 end
