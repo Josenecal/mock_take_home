72a2d9016bbb223b5a9f8ec73cdf9a2e2f3917d3
Joseph Senecal <jasenecal2@gmail.com>
2022-08-03 10:19:49 -0600

    Test: add relationship tests to customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   context 'validations and relations' do
-    it { should valid_presence_of :first_name }
-    it { should valid_presence_of :last_name }
-    it { should valid_presence_of :email }
-    it { should valid_presence_of :address }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
+    it { should validate_uniqueness_of :email }
+    
+    it { should have_many :subscriptions }
+    it { should have_many :teas, through: :subscriptions }
   end
 end
