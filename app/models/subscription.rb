ddcee8c4c19c81c73c078c962c458657804c9ab3
Joseph Senecal <jasenecal2@gmail.com>
2022-08-03 19:57:43 -0600

    Feat: add enums to subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
+    enum status: [ :cancelled, :active ]
+    enum frequency: [ :weekly, :semi_weekly, :monthly, :bi_monthly, :semi_monthly, :semi_anually]
+
+
     belongs_to :customer
     belongs_to :tea
 
