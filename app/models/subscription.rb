19d11367e8406b5a0c9120e99f47e0f50dfc1ccd
Joseph Senecal <jasenecal2@gmail.com>
2022-08-04 23:23:55 -0600

    Feat: add method to subscription to assign price dynamically

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,4 +7,16 @@ class Subscription < ApplicationRecord
     belongs_to :tea
 
     validates_presence_of :title, :description, :frequency, :price, :status
+
+    def self.price_setter(freq)
+        price_to_enum_hash = {
+            "weekly" => 3500,
+            "semi_weekly" => 1800,
+            "monthly" => 1250,
+            "semi_monthly" => 1500,
+            "bi_monthly" => 1000,
+            "semi_anually" => 800
+        }
+        return price_to_enum_hash[freq]
+    end
 end
