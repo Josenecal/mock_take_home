430d9bdd778674470cf1c9b72e01ee30269c997d
Joseph Senecal <jasenecal2@gmail.com>
2022-08-04 23:37:05 -0600

    Ref: change delete to post to be more restful

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'api/v1/subscriptions', to: 'api/v1/subscriptions#index'
   post 'api/v1/subscriptions', to: 'api/v1/subscriptions#create'
-  delete 'api/v1/subscriptions', to: 'api/v1/subscriptions#delete'
+  patch 'api/v1/subscriptions', to: 'api/v1/subscriptions#cancel'
 end
