1a0dda7299579892cba1b91bd032995b5036932d
Joseph Senecal <jasenecal2@gmail.com>
2022-08-04 22:12:22 -0600

    Test: start tests for new subscription endpoint

---
+++ spec/requests/api/v1/post_new_subscription_endpoint_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Subscriptions", type: :request do
+  describe "POST /create" do
+    before :each do
+        @customer_1 = Customer.create!(first_name: "Cedric", last_name: "Diggory", email: "digs-a-lot@yahoo.net", address: "130 Green Acres Dr, Burlington, VT, 05401" )
+        @tea_1 = Tea.create!(title: "Gen Mai Cha", description: "nutty and roasty", temperature: 86, brew_time: 210)
+    end
+
+    it "accepts a customer_id and a tea_id parameter and creates (and returns) that new subscription object" do
+        post "http://localhost:300/api/v1/subscriptions/", params: {tea_id: @tea_1.id, customer_id: @customer_1.id}
+        response_body = JSON.parse(response.body, symbolize_names: true)
+        binding.pry
+    end
+    
+  end
+end
